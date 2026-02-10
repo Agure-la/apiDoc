e6c614ec86e7ee9f3f2f0d924b49a7a01d9a623e
Agure-la <lameckagure@gmail.com>
2026-02-10 13:11:14 +0300

    added auth table

---
+++ internal/spec/auth.go
@@ -0,0 +1,16 @@
+package spec
+
+type AuthScheme struct {
+	Type        AuthType `json:"type"`
+	Name        string   `json:"name,omitempty"`
+	Description string   `json:"description,omitempty"`
+}
+
+type AuthType string
+
+const (
+	AuthAPIKey AuthType = "apiKey"
+	AuthOAuth2 AuthType = "oauth2"
+	AuthJWT    AuthType = "jwt"
+	AuthNone   AuthType = "none"
+)
